b0dd5bf4ea7933629b6babda1e15db10fc365578
Karan Sequeira <u1087117@ad.utah.edu>
2016-09-27 17:44:36 -0600

    Fixed a bug with the verbose print function...

--- Engine/Source/Logger/Logger.h
+++ Engine/Source/Logger/Logger.h
@@ -3,9 +3,21 @@
 
 #include <assert.h>
 
+// Define varying levels of verbosity
+// At 0, simply print a message
+// At 1, print file name and line number ahead of the message
+#define DEBUG_LOG_LEVEL 0
+
 #if defined(BUILD_DEBUG)
 	#define ASSERT(cond)					assert((cond))
-	#define LOG_DEBUG(format, ...)			engine::Print((format), __VA_ARGS__)
+
+	#if defined(DEBUG_LOG_LEVEL) && (DEBUG_LOG_LEVEL == 1)
+		#define LOG_DEBUG(format, ...)			engine::Print(__FUNCTION__, __LINE__, (format), __VA_ARGS__)
+	#elif defined(DEBUG_LOG_LEVEL) && (DEBUG_LOG_LEVEL == 0)
+		#define LOG_DEBUG(format, ...)			engine::Print((format), __VA_ARGS__)
+	#else
+		#define LOG_DEBUG(format, ...)			engine::Print((format), __VA_ARGS__)
+	#endif
 #else
 	#define ASSERT(cond)					void(0)
 	#define LOG_DEBUG(format, ...)			void(0)
@@ -14,6 +26,7 @@
 namespace engine
 {
 	void Print(const char* format, ...);
+	void Print(const char* function_name, const int line_number, const char* format, ...);
 } // namespace engine
 
 #endif // ENGINE_LOGGER_H_
--- Engine/Source/Logger/Private/Logger.win32.cpp
+++ Engine/Source/Logger/Private/Logger.win32.cpp
@@ -8,12 +8,29 @@ namespace engine
 	void Print(const char* format, ...)
 	{
 		const size_t len_temp = 256;
-		char str_temp[len_temp] = "DEBUG: ";
+		char str_temp[len_temp] = { 0 };
 
-#if defined(DEBUG_LOG_LEVEL) && (DEBUG_LOG_LEVEL == 1)
-		sprintf_s(str_temp, "DEBUG-%s-%d:", __FUNCTION__, __LINE__);
-#endif
+		sprintf_s(str_temp, "DEBUG: ");
+		strcat_s(str_temp, format);
+		strcat_s(str_temp, "\n");
 
+		const size_t len_output = len_temp + 32;
+		char str_output[len_output] = { 0 };
+
+		va_list args;
+		va_start(args, format);
+		vsprintf_s(str_output, len_output, str_temp, args);
+		va_end(args);
+
+		OutputDebugStringA(str_output);
+	}
+
+	void Print(const char* function_name, const int line_number, const char* format, ...)
+	{
+		const size_t len_temp = 256;
+		char str_temp[len_temp] = { 0 };
+
+		sprintf_s(str_temp, "VERBOSE - %s - %d: ", function_name, line_number);
 		strcat_s(str_temp, format);
 		strcat_s(str_temp, "\n");
 
