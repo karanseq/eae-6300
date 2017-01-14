d8d7babc7fa525087c160cdebab13dd3c2f71add
Karan Sequeira <u1087117@ad.utah.edu>
2017-01-13 19:00:56 -0700

    Integrated GLib...

--- MonsterChase/Source/Game/Private/Main.cpp
+++ MonsterChase/Source/Game/Private/Main.cpp
@@ -1,9 +1,12 @@
 // library includes
+#include <Windows.h>
 #ifdef BUILD_DEBUG
 #include <crtdbg.h>
 #endif // BUILD_DEBUG
 
 // engine includes
+#include "Assert\Assert.h"
+#include "GLib.h"
 #include "Logger\Logger.h"
 #include "Memory\AllocatorUtil.h"
 
@@ -12,14 +15,19 @@
 
 void RunTests();
 
-int main(int* i_argv, char** i_argc)
+int WINAPI wWinMain( HINSTANCE i_h_instance, HINSTANCE i_h_prev_instance, LPWSTR i_lp_cmd_line, int i_n_cmd_show )
 {
+	// initialize allocators
 	engine::memory::CreateAllocators();
 	
+	// initialize GLib
+	bool success = GLib::Initialize(i_h_instance, i_n_cmd_show, "MonsterChase", -1, 800, 600);
+	ASSERT(success);
+
 	RunTests();
 	LOG("\n");
 
-	LOG("-------------------- Running MonsterChase --------------------");
+	/*LOG("-------------------- Running MonsterChase --------------------");
 	MonsterChase* monster_chase = new MonsterChase();
 
 	while (monster_chase->GetState() != GameStates::kGameStateQuit)
@@ -30,8 +38,12 @@ int main(int* i_argv, char** i_argc)
 	delete monster_chase;
 	monster_chase = nullptr;
 
-	LOG("-------------------- Finished MonsterChase --------------------\n\n");
+	LOG("-------------------- Finished MonsterChase --------------------\n\n");*/
 
+	// cleanup GLib
+	GLib::Shutdown();
+
+	// cleanup allocators
 	engine::memory::DestroyAllocators();
 
 #if defined(_DEBUG)
